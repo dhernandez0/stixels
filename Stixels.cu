6a389143cc250de4301aadc0569216b7a00ec5ac
dhernandez <dhernandez@caos.uab.es>
2016-10-19 17:59:05 +0200

    - Added license to all files

--- Stixels.cu
+++ Stixels.cu
@@ -1,14 +1,25 @@
-/*
- * Stixels.cpp
- *
- *  Created on: Feb 17, 2016
- *      Author: dani
- */
+/**
+    This file is part of stixels. (https://github.com/dhernandez0/stixels).
+
+    Copyright (c) 2016 Daniel Hernandez Juarez.
+
+    stixels is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    stixels is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with stixels.  If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
 #include "Stixels.hpp"
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
 Stixels::Stixels() {
 }
 
@@ -16,9 +27,6 @@ Stixels::~Stixels() {
 }
 
 
-//////////////////////////////////////////////////////////////////////
-// Initialization/Finalization
-//////////////////////////////////////////////////////////////////////
 void Stixels::Initialize() {
 	m_disp_im_modified = m_disp_im;
 	m_realcols = (m_cols-m_width_margin)/m_column_step;
